24961e9e951d75f99932b37be37368be94dc1258
Ernad Husremovic <hernad@bring.out.ba>
2016-01-31 19:33:07 +0100

    fix xtdesktop

--- sql/0009_drop_xt_schemas.up.sql
+++ sql/0009_drop_xt_schemas.up.sql
@@ -1,4 +1,4 @@
 drop schema if exists xtpos cascade;
-drop schema if exists xdesktop cascade;
+drop schema if exists xtdesktop cascade;
 drop schema if exists te cascade;
 drop schema if exists u2 cascade;
