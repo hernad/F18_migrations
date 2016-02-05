6a52571c4872f5555edffb8693933e1885c19bce
Ernad Husremovic <hernad@bring.out.ba>
2016-02-05 11:18:05 +0100

    run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-export F18_DB=rg_2015
-export F18_MIGRATE_URL=postgres://postgres@localhost:5432/$F18_DB?sslmode=disable
+
+if [ -z $1 ] ; then
+  echo "usage $0  <host> <user> <pass> <db>"
+  exit 1
+fi 
+
+HOST=${1:-localhost}
+USER=${2:-postgres}
+PASS=${3:-postgres}
+F18_DB=${4:-test}
+
+export F18_DB
+
+export F18_MIGRATE_URL=postgres://$USER:$PASS@$HOST:5432/$F18_DB?sslmode=disable
 
 migrate -url $F18_MIGRATE_URL -path ./sql up
 migrate -url $F18_MIGRATE_URL -path ./sql version
