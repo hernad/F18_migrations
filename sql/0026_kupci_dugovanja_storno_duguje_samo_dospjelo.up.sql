470b5b48aa434e0962abc880e7d4de58456ec1bd
Ernad Husremovic <hernad@bring.out.ba>
2016-09-05 14:53:33 +0200

    storno duguje samo nedospjelo

--- sql/0026_kupci_dugovanja_storno_duguje_samo_dospjelo.up.sql
+++ sql/0026_kupci_dugovanja_storno_duguje_samo_dospjelo.up.sql
@@ -1,5 +1,3 @@
--- SELECT * from sp_duguje_stanje_2( RPad( '2110', 7 ), RPad( 'TEST', 6) , '2016-01-01', '2016-12-31')
-
 CREATE OR REPLACE FUNCTION sp_duguje_stanje_2( param_konto varchar(7), param_partner varchar(6), param_dat_od date, param_dat_do date,
 OUT pocstanje double precision,
 OUT dospjelo double precision, OUT nedospjelo double precision, OUT valuta date )
@@ -36,24 +34,22 @@ EXECUTE 'SELECT sum(CASE WHEN d_p=''1'' THEN iznosbhd ELSE -iznosbhd END) FROM '
    ''' AND '''  || param_dat_do || ''' and otvst=''9''' INTO row;
 
 nDospjelo := COALESCE( row.sum, 0);
-
 --RAISE NOTICE 'suma zatvorenih stavki %', row.sum;
 
--- suma negativnih stavki - storno duguje, ili potrazuje
+-- suma negativnih stavki storno duguje koje su dospjele na dan
 EXECUTE 'SELECT sum(CASE WHEN d_p=''1'' THEN iznosbhd ELSE -iznosbhd END) FROM '  || table_name || ' WHERE idkonto = '''  || param_konto ||
    ''' AND idpartner = ''' || param_partner || ''' AND datdok BETWEEN ''' || param_dat_od ||
    ''' AND '''  || param_dat_do
-   || ''' AND ( ((d_p=''1'' AND iznosbhd<0) OR (d_p=''2'' AND iznosbhd>0)) AND COALESCE(datval,datdok)<'''
-   || param_dat_do || ''') AND otvst='' ''' INTO row;
+   || ''' AND (d_p=''1'' AND iznosbhd<0) AND coalesce(datval,datdok)<='''
+   || param_dat_do || ''' AND otvst='' ''' INTO row;
 nDospjelo := nDospjelo + COALESCE( row.sum, 0);
-
---RAISE NOTICE 'suma negativnih stavki %', row.sum;
+--RAISE NOTICE 'suma negativnih stavki dospjelo % na dan %', row.sum, param_dat_do;
 
 FOR row IN
-  -- samo pozitivne stavke duguju trebamo: AND NOT (d_p='' '' AND iznosbhd<0)
+  -- sve stavke osim storno duguju koje su vec obuhvacene: AND NOT (d_p=''1'' AND iznosbhd<0) AND COALESCE(datval,datdok)<= ...)
   EXECUTE 'SELECT iznosbhd,datval,datdok,d_p,idvn,otvst,brdok FROM '  || table_name || ' WHERE idkonto = '''  || param_konto ||
    ''' AND idpartner = ''' || param_partner || ''' AND datdok BETWEEN ''' || param_dat_od ||
-   ''' AND '''  || param_dat_do || ''' AND NOT ((d_p=''1'' AND iznosbhd<0) OR (d_p=''2'' AND iznosbhd>0) AND COALESCE(datval,datdok)<''' || param_dat_do || ''') AND otvst='' '' ORDER BY idfirma,idkonto,idpartner,datdok,brdok'
+   ''' AND '''  || param_dat_do || ''' AND NOT ((d_p=''1'' AND iznosbhd<0) AND COALESCE(datval,datdok)<=''' || param_dat_do || ''') AND otvst='' '' ORDER BY idfirma,idkonto,idpartner,datdok,brdok'
 LOOP
 
 nCnt := nCnt + 1;
@@ -64,22 +60,25 @@ nRowIznos := COALESCE(row.iznosbhd, 0);
 
 IF (row.d_p = '1') THEN
 
-   IF row.idvn <> '01' AND (nRowIznos > 0) AND (dValuta > dRowValuta) THEN
+   IF (nRowIznos > 0) AND (dValuta > dRowValuta) THEN
         --RAISE NOTICE 'set valuta prve otvorene stavke - otvorene stavke sa najnizim datumom set valuta: % tekuca valuta: %', dValuta, dRowValuta;
         dValuta :=  dRowValuta;
    END IF;
 
   IF dRowValuta > param_dat_do  THEN -- nije dospijelo do dat_do
      nNeDospjelo := nNeDospjelo + nRowIznos;
-
   ELSE
      nDospjelo := nDospjelo + nRowIznos;
-
   END IF;
 
 ELSE
-  --IF (row.d_p = '2') THEN  -- uplata
-  nDospjelo := nDospjelo - nRowIznos;
+  --IF (row.d_p = '2') THEN  -- potrazuje -> uplata, ili storno izlaza
+  IF dRowValuta > param_dat_do  THEN
+     nNeDospjelo := nNeDospjelo - nRowIznos;
+  ELSE
+     nDospjelo := nDospjelo - nRowIznos;
+  END IF;
+
 END IF;
 
 IF ( nStanjePredhodno < 0) AND (dValuta < dRowValuta)  THEN
